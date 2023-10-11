39ad29857e0b6191dae1ef0e77c15771da447065
Łukasz Dudek <lukasz.dudek.94@gmail.com>
2023-10-12 00:16:40 +0200

    add css styles to generater, separate componenents, create headers sections WIP

--- src/generator/generator.repo.ts
+++ src/generator/generator.repo.ts
@@ -15,25 +15,50 @@ export class GeneratorRepository {
   }
 
 
-  static async generatePage(body: { componentsIds: number[]}): Promise<void> {
-      body[0];
+  static async generatePage(body: { componentsIds: number[] }): Promise<void> {
+    body[0];
 
-      const layout = readFileSync('html-templates/layout.html', 'utf-8');
+    const version = 'ver1';
 
-      const headerContent = readFileSync('html-templates/components/header.html', 'utf-8');
-      const footerContent = readFileSync('html-templates/components/footer.html', 'utf-8');
+    try {
+      const mainStylesCSS = readFileSync(`html-templates/${version}/styles/main-styles`, 'utf-8');
+      const layoutHTML = readFileSync('html-templates/layout.html', 'utf-8');
+      const pageTemplate = Handlebars.compile(layoutHTML);
+      const mainStylesTemplate = Handlebars.compile(mainStylesCSS);
 
-      const template = Handlebars.compile(layout);
-      const generatedPage = template({
-        title: 'Generated Page',
-        backgroundColor: 'rgb(100, 129, 133);',
-        header: headerContent,
-        footer: footerContent
+      const generatedPage = pageTemplate({
+        content: {
+          header: {
+            title: 'Generated Page LPG',
+          }
+        },
+        components: {
+          header: {
+            desktop: readFileSync(`html-templates/${version}/components/header/header-desktop.html`, 'utf-8'),
+            mobile: readFileSync(`html-templates/${version}/components/header/header-mobile.html`, 'utf-8')
+          },
+          footer: readFileSync(`html-templates/${version}/components/footer/footer.html`, 'utf-8')
+        }
       });
+
+
+      const generatedMainStyle = mainStylesTemplate({
+        body: {
+          background: 'rgb(100, 129, 133)'
+        }
+      });
+
 
       console.log('Generated!');
 
+
       writeFileSync('generated/generated-page.html', generatedPage);
+      writeFileSync('generated/styles/main-styles.css', generatedMainStyle);
+
+    }
+    catch (err) {
+      console.log(err);
+    }
 
   }
 }
