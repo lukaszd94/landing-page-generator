5bbecce823daedb8955c51b11aac426333726d1f
Łukasz Dudek <lukasz.dudek.94@gmail.com>
2023-11-09 00:34:13 +0100

    add code editor react component

--- frontend/src/components/page-components/page-components.js
+++ frontend/src/components/page-components/page-components.js
@@ -1,20 +1,48 @@
 import './page-components.scss';
 import axios from 'axios';
+import Editor from "@monaco-editor/react";
+import { useState, useEffect } from 'react';
 
 function PageComponents() {
 
-  function getComponent(pageComponentId) {
-    axios.get(`http://localhost:8081/api/generator/page-components/${pageComponentId}`, {})
-      .then(response => {
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.log('Error:', error);
-      });
+  const [pageComponent, setPageComponent] = useState({
+    id: null,
+    pageId: null,
+    type: null,
+    name: null,
+    htmlCode: null,
+    cssCode: null,
+    jsCode: null,
+    htmlVars: null,
+    cssVars: null,
+    jsVars: null,
+  });
+
+  async function getComponent(pageComponentId) {
+    const {data} = await axios.get(`http://localhost:8081/api/generator/page-components/${pageComponentId}`);
+    const pageComp = data.payload[0];
+
+    return setPageComponent(prevState => {
+      return {
+        id: pageComp.id,
+        pageId: pageComp.pageId,
+        type: pageComp.type,
+        name: pageComp.name,
+        htmlCode: pageComp.htmlCode,
+        cssCode: pageComp.cssCode,
+        jsCode: pageComp.jsCode,
+        htmlVars: pageComp.htmlVars,
+        cssVars: pageComp.cssVars,
+        jsVars: pageComp.jsVars
+      }
+    });
   }
 
+  useEffect(() => {
+    console.log('getComponent')
+    getComponent(2);
+  }, []);
 
-  getComponent(2);
 
 
   return (
@@ -23,21 +51,54 @@ function PageComponents() {
         <div>
           <h4>HTML</h4>
           <div>
-            <textarea name="" id="" cols="50" rows="10"></textarea>
+            <Editor
+              height="200px"
+              width="500px"
+              language="html"
+              theme="vs-dark"
+              value={pageComponent.htmlCode}
+              options={{
+                fontSize: "12px",
+                formatOnType: true,
+                autoClosingBrackets: true
+              }}
+            />
           </div>
         </div>
 
         <div>
           <h4>CSS</h4>
           <div>
-            <textarea name="" id="" cols="50" rows="10"></textarea>
+          <Editor
+              height="200px"
+              width="500px"
+              language="scss"
+              theme="vs-dark"
+              value={pageComponent.cssCode}
+              options={{
+                fontSize: "12px",
+                formatOnType: true,
+                autoClosingBrackets: true
+              }}
+            />
           </div>
         </div>
 
         <div>
           <h4>JS</h4>
           <div>
-            <textarea name="" id="" cols="50" rows="10"></textarea>
+          <Editor
+              height="200px"
+              width="500px"
+              language="javascript"
+              theme="vs-dark"
+              value={pageComponent.jsCode}
+              options={{
+                fontSize: "12px",
+                formatOnType: true,
+                autoClosingBrackets: true
+              }}
+            />
           </div>
         </div>
       </div>
